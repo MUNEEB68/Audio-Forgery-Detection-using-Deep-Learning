91283c1f78cb8b5d098c8151cf73cc3b9c3e9180
Muneeb Haroon <muneebharoon261@gmail.com>
2025-12-01 01:47:01 +0500

    recovered code

--- ResNet18_Finetune/final_label_extraction.py
+++ ResNet18_Finetune/final_label_extraction.py
@@ -11,6 +11,9 @@ with open(label_file, 'r') as f_in, open(target_file, 'w') as f_out:
             continue
         parts = line.split()
         label = parts[-1]  # last element is the label
-        f_out.write(f"{label}\n")
+        name_of_audio_file = parts[0] # first element is the audio file name
+        f_out.write(f"{name_of_audio_file}.wav {label}\n")
+
+        
 
 print(f"Target labels saved to: {target_file}")
